740df6e080d4c125dd39bf6fcfc1ece68bb77378
Cristian Bernal <cristian.bernal.ortega@gmail.com>
2024-07-12 22:53:06 -0400

    arreglo redirecciones

--- ExamenTransversal/Biblioteca/urls.py
+++ ExamenTransversal/Biblioteca/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('index', views.index, name="index"),
-    path('autores', views.autores, name="autores"),
-    path('categorias', views.categorias, name="categorias"),
-    path('libros', views.libros, name="libros"),
+    path('index/', views.index, name="index"),
+    path('autores/', views.autores, name="autores"),
+    path('categorias/', views.categorias, name="categorias"),
+    path('libros/', views.libros, name="libros"),
 ]
