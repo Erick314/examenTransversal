d6c23a8272836b67bf0fdbbdd15685ee2a1c0029
Erick <eric.paredes@duocuc.cl>
2024-07-13 01:16:41 -0400

    buscador casi

--- ExamenTransversal/Biblioteca/views.py
+++ ExamenTransversal/Biblioteca/views.py
@@ -14,23 +14,13 @@ def categorias(request):
     return render(request, 'Biblioteca/categorias.html', context)
 
 def libros(request):
-    context = {}
+    query = request.GET.get('q')  # Obtiene el término de búsqueda de la solicitud GET
+    if query:
+        libros = Libro.objects.filter(titulo__icontains=query)  # Filtra los libros que contienen el término en el título
+    else:
+        libros = Libro.objects.all()  # Muestra todos los libros si no hay término de búsqueda
+    
+    context = {
+        'libros': libros  # Pasa los libros filtrados o todos los libros a la plantilla
+    }
     return render(request, 'Biblioteca/libros.html', context)
-
-def buscar_libros(request):
-    query = request.GET.get('query', '')
-    libros = Libro.objects.filter(
-        models.Q(tituloicontains=query) |
-        models.Q(anioPublicacionicontains=query) |
-        models.Q(descripcion__icontains=query)
-    )
-    results = [
-        {
-            'idLibro': libro.idLibro,
-            'titulo': libro.titulo,
-            'anioPublicacion': libro.anioPublicacion,
-            'descripcion': libro.descripcion
-        }
-        for libro in libros
-    ]
-    return JsonResponse(results, safe=False)
